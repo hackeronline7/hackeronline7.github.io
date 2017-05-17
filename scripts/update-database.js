05025cec872ae37973cdc6dd9a39e0d4bfbd32ff
hackeronline7 <onlinetohackyou@gmail.com>
2017-05-18 04:40:30 +0530

    Delete update-database.js
    not required

--- scripts/update-database.js
+++
@@ -1,7 +0,0 @@
-var editor = ace.edit('editor');
-var checkPoint = setInterval(function(){
-    var codeText = editor.getValue();
-    //console.log(codeText);
-    firebase.database().ref('code').set(codeText);
-}, 500); 
-
