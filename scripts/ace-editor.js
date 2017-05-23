2aa95356715c179773f5314c557c62aaef44bc45
hackeronline7 <onlinetohackyou@gmail.com>
2017-05-23 13:49:37 +0530

    Update ace-editor.js

--- scripts/ace-editor.js
+++ scripts/ace-editor.js
@@ -1,21 +1,4 @@
 var editor = ace.edit("editor");
 editor.setTheme("ace/theme/twilight");
-
-//var tp = { "action":"insert", "end":{ "column": "1", "row" :"0"}, "lines":{"0":"h"},"start":{ "column": "0", "row" :"0"} };
-//editor.getSession().getDocument().applyDeltas(tp);
-
-//var tp;
-
-//editor.on('change', function(e){
-  // if(!tp){
-  //     tp = e;
-  // }      
-//editor.getSession().getDocument().applyDeltas([tp]);
-//});
-
-
-
-
-
 //editor.session.setMode("ace/mode/javascript");
 //editor.$blockScrolling = Infinity;
