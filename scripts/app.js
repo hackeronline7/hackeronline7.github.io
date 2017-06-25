3982f8965464e52e258098224887aacfef3bfee8
hackeronline7 <onlinetohackyou@gmail.com>
2017-06-26 02:30:06 +0530

    Update app.js

--- scripts/app.js
+++ scripts/app.js
@@ -22,8 +22,8 @@
         document.cookie = Math.random().toString(36).substr(2, 9);
     }
     
-    console.log(editor.getValue());
-    if(editor.getValue() !== ""){
+    console.log(dbCode.val());
+    if(dbCode.val()){
         dbCode.once('value', function(ref){
         editor.setValue(ref.val(),-1);
         initialRead = true;
